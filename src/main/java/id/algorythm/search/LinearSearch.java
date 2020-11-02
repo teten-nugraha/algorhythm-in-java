e3cc0071c3d7d1abde478e113072ed3d32b7a6e1
Teten Nugraha <teten.nugraha@allianz.co.id>
2020-11-02 07:46:51 +0700

    Add Linear Search

---
+++ src/main/java/id/algorythm/search/LinearSearch.java
@@ -0,0 +1,19 @@
+package id.algorythm.search;
+
+public class LinearSearch {
+
+    public static void main(String args[]){
+        int array[] = {10, 20, 25, 63, 96, 57};
+        int size = array.length;
+        int value = 63;
+
+        for (int i=0 ;i< size-1; i++){
+            if(array[i]==value){
+                System.out.println("Element found index is :"+ i);
+            }else{
+                System.out.println("Element not found");
+            }
+        }
+    }
+
+}
